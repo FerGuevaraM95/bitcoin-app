e87b27dee0ba9c0db8945b08e240111401769eb4
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-08-17 17:45:38 -0500

    Add navbar

--- .next/server/bundles/pages/index.js
+++ .next/server/bundles/pages/index.js
@@ -151,20 +151,17 @@ var _jsxFileName = "/Users/fernandoguevaramarquez/Desktop/bitcoin-app/components
 
 
 var Navigation = function Navigation() {
-  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("nav", {
+    className: "navbar navbar-expand navbar-dark bg-warning",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 4
     }
-  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("ul", {
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    className: "container",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 5
-    }
-  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("li", {
-    __source: {
-      fileName: _jsxFileName,
-      lineNumber: 6
     }
   }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_next_link___default.a, {
     href: "/",
@@ -173,27 +170,60 @@ var Navigation = function Navigation() {
       lineNumber: 6
     }
   }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("a", {
+    className: "navbar-brand",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 6
     }
-  }, "Inicio"))), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("li", {
+  }, "TodoBitcoin")), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    className: "collapse navbar-collapse",
     __source: {
       fileName: _jsxFileName,
       lineNumber: 7
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    className: "navbar-nav ml-auto",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("li", {
+    className: "nav-item",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_next_link___default.a, {
+    href: "/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("a", {
+    className: "nav-link",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, "Inicio"))), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("li", {
+    className: "nav-item",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
     }
   }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_next_link___default.a, {
     href: "/about",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
+      lineNumber: 13
     }
   }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("a", {
+    className: "nav-link",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
+      lineNumber: 13
     }
-  }, "Nosotros")))));
+  }, "Nosotros")))))));
 };
 
 /* harmony default export */ __webpack_exports__["a"] = (Navigation);
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,12 +1,21 @@
 import Link from 'next/link';
 
 const Navigation = () => (
-    <div>
-        <ul>
-            <li><Link href="/"><a>Inicio</a></Link></li>
-            <li><Link href="/about"><a>Nosotros</a></Link></li>
-        </ul>
-    </div>
+    <nav className="navbar navbar-expand navbar-dark bg-warning">
+        <div className="container">
+            <Link href="/"><a className="navbar-brand">TodoBitcoin</a></Link>
+            <div className="collapse navbar-collapse">
+                <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link href="/"><a className="nav-link">Inicio</a></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/about"><a className="nav-link">Nosotros</a></Link>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </nav>
 )
 
 export default Navigation;
